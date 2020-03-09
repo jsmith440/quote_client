709eca4c8988528cf97c241f2fffe0893b097b19
jsmith440 <jsmith440@cnm.edu>
2020-03-09 15:15:21 -0600

    OAuth Key

--- app/src/main/java/edu/cnm/deepdive/quoteclient/service/QuoteRepository.java
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/service/QuoteRepository.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.quoteclient.service;
 
 import edu.cnm.deepdive.quoteclient.model.Quote;
+import edu.cnm.deepdive.quoteclient.service.QodService.InstanceHolder;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 import java.util.concurrent.Executor;
@@ -9,6 +10,7 @@ import java.util.concurrent.Executors;
 public class QuoteRepository {
 
   private static final int NETWORK_POOL_SIZE = 10;
+  private static final String OAUTH_HEADER_FORMAT = "Bearer %s";
 
   private final QodService proxy;
   private final Executor networkPool;
@@ -17,13 +19,12 @@ public class QuoteRepository {
     proxy = QodService.getInstance();
     networkPool = Executors.newFixedThreadPool(NETWORK_POOL_SIZE);
   }
-
   public static QuoteRepository getInstance() {
     return InstanceHolder.INSTANCE;
   }
 
-  public Single<Quote> getRandom() {
-    return proxy.getRandom()
+  public Single<Quote> getRandom(String token) {
+    return proxy.getRandom(String.format(OAUTH_HEADER_FORMAT,token))
         .subscribeOn(Schedulers.from(networkPool));
   }
 
